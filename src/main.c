485517e01742bf138d19d305826a19880cf7f55a
Lorenz Heider <studheidlo5538@stud.th-rosenheim.de>
2023-11-06 18:17:36 +0100

    Programm fertig

--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#define BREITE 0
 
 int main()
 {
-    printf("Test");
+    float breite = BREITE;
+    float breitefix = 0;
+    float leerzeichen = 0;
+
+    printf("Bitte geben sie die Breite des Quadrats an:");
+    scanf("%f", &breite);
+    breitefix = breite;
+
+    breite = breite * 2.7;
+    while ( breite > 0)
+    {
+        printf("*");
+        breite = breite - 1;
+    }
+
+    printf("\n");
+
+    breite = breitefix - 2;
+    while (breite > 0)
+    {
+        leerzeichen = breitefix * 2.7 - 2;
+        printf("*");
+        while (leerzeichen > 0)
+        {
+            printf(" ");
+            leerzeichen = leerzeichen - 1;
+        }
+        printf("*\n");
+        breite = breite - 1;
+    }
+
+    breite = breitefix * 2.7;
+    while (breite > 0)
+    {
+        printf("*");
+        breite = breite - 1;
+    }
+    
     return 0;
 }
